eb95509fde93a5a93803ed6f520aafa2e4c00740
Raaina <raainawhalla83@gmail.com>
2024-06-10 22:26:05 +0530

    "Set up database"

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,59 +1,90 @@
-const users = [
-    {
-        id:1,
-        name: "Raaina"
-    },
-    {
-        id:2,
-        name: "Priya"
-    },
-    {
-        id:3,
-        name: "Preti"
-    }
-]
+import connectToDB from "./connectTodb"
+import { Post, User } from "./models"
 
-const posts = [
-    {
-        userId: 1,
-        id: 1,
-        title: "jwgui dwguo",
-        body: "bcuig jwugu bdiwugu hduiwh"
-    },
-    {
-        userId: 2,
-        id: 2,
-        title: "jwgui dwguo",
-        body: "bcuig jwugu bdiwugu hduiwh"
-    },
-    {
-        userId: 1,
-        id: 3,
-        title: "jwgui dwguo",
-        body: "bcuig jwugu bdiwugu hduiwh"
-    },
-    {
-        userId: 3,
-        id: 4,
-        title: "jwgui dwguo",
-        body: "bcuig jwugu bdiwugu hduiwh"
-    },
-    {
-        userId: 2,
-        id: 5,
-        title: "jwgui dwguo",
-        body: "bcuig jwugu bdiwugu hduiwh"
-    },
-]
+// const posts = [
+//     {
+//         userId: 1,
+//         id: 1,
+//         title: "jwgui dwguo",
+//         body: "bcuig jwugu bdiwugu hduiwh"
+//     },
+//     {
+//         userId: 2,
+//         id: 2,
+//         title: "jwgui dwguo",
+//         body: "bcuig jwugu bdiwugu hduiwh"
+//     },
+//     {
+//         userId: 1,
+//         id: 3,
+//         title: "jwgui dwguo",
+//         body: "bcuig jwugu bdiwugu hduiwh"
+//     },
+//     {
+//         userId: 3,
+//         id: 4,
+//         title: "jwgui dwguo",
+//         body: "bcuig jwugu bdiwugu hduiwh"
+//     },
+//     {
+//         userId: 2,
+//         id: 5,
+//         title: "jwgui dwguo",
+//         body: "bcuig jwugu bdiwugu hduiwh"
+//     },
+// ]
+
+// const users = [
+//     {
+//         id:1,
+//         name: "Raaina"
+//     },
+//     {
+//         id:2,
+//         name: "Priya"
+//     },
+//     {
+//         id:3,
+//         name: "Preti"
+//     }
+// ]
 
 export const getPosts = async () => {
-    return posts
+    try {
+        connectToDB()
+        const posts = Post.find({})
+        return posts
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const getSinglePost = async(id) => {
-    return posts.find((post) => post.id === parseInt(id))
+export const getSinglePost = async(slug) => {
+    try {
+        connectToDB()
+        const post = Post.find({ slug })
+        return post
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export const getUser = async(userId) => {
-    return users.find((user) => user.id === userId)
+    try {
+        connectToDB()
+        const user = User.findById(userId)
+        return user
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getAllUsers = async(userId) => {
+    try {
+        connectToDB()
+        const users = User.find({})
+        return users
+    } catch (error) {
+        console.log(error)
+    }
 }
---
+++ src/lib/models.js
@@ -0,0 +1,56 @@
+import  mongoose  from "mongoose";
+
+const userSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        min: 3,
+        max: 15
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max:15
+    },
+    profile: {
+        type: String,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+}, {timestamps: true})
+
+const postSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        max: 100,
+    },
+    img: {
+        type: String,
+    },
+    userId: {
+        type: Boolean,
+        required: true,
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, {timestamps: true})
+
+export const User = mongoose.models.User || mongoose.model("User", userSchema)
+export const Post = mongoose.models.Post || mongoose.model("Post", postSchema)
